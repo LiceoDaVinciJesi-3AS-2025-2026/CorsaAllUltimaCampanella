372bbed6797b22eaae7932fdfcdeddae7201952e
maty <bottegonimaty@gmail.com>
2026-02-18 19:52:46 +0100

    aggiunto la schermata personaggi con sfondo e la schermata start

--- src/corsaallultimacampanella/__init__.py
+++ src/corsaallultimacampanella/__init__.py
@@ -17,15 +17,22 @@ buttonStart = pygame.Rect(SCREEN_WIDTH // 2.399, SCREEN_HEIGHT // 1.235, 280, 80
 #bottone per visualizzare i personaggi
 buttonPersonaggi = pygame.Rect(SCREEN_WIDTH // 2.4, SCREEN_HEIGHT // 1.48, 280, 80)
 
-imgSfondo = pygame.image.load("pixelscuola.png") 
-imgSfondo = pygame.transform.scale(imgSfondo,(SCREEN_WIDTH,SCREEN_HEIGHT))
+imgSfondoPrincipale = pygame.image.load("pixelscuola.png") 
+imgSfondoPrincipale = pygame.transform.scale(imgSfondoPrincipale,(SCREEN_WIDTH,SCREEN_HEIGHT))
 
+imgSfondoPersonaggi = pygame.image.load("portapersonaggi.png")
+imgSfondoPersonaggi = pygame.transform.scale(imgSfondoPersonaggi,(SCREEN_WIDTH,SCREEN_HEIGHT))
 
 Normalfont = pygame.font.SysFont('Impact', 60)
 parolaButtonStart = Normalfont.render("start", True, "black")
 parolaButtonPersonaggi = Normalfont.render("personaggi", True, "black")
 
+clock = pygame.time.Clock()
+
 running = True
+home = True
+personaggi = True 
+gioco = True
 
 while running:
     mPos = pygame.mouse.get_pos() 
@@ -33,32 +40,51 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-        if event.type == pygame.MOUSEBUTTONDOWN: 
-            if buttonStart.collidepoint(mPos):
-                running = True
-        if event.type == pygame.MOUSEBUTTONDOWN: 
-            if buttonPersonaggi.collidepoint(mPos):
-                running = True
                 
-    screen.fill("green")
-    screen.blit(imgSfondo,(0,0) )
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1: #click sinistro 
+                if home and buttonStart.collidepoint(mPos):
+                    home = False
+                    personaggi = False
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1: #click sinistro 
+                if personaggi and buttonPersonaggi.collidepoint(mPos):
+                    home = False
+                    gioco = False
 
-    buttonColorS = "white"
-    if buttonStart.collidepoint(mPos):
-        buttonColorS = "grey"
-    buttonS = pygame.draw.rect(screen,buttonColorS,buttonStart)
-    screen.blit(parolaButtonStart, (SCREEN_WIDTH // 2.18, SCREEN_HEIGHT // 1.235))
+    #se ci troviamo nella schermata home
+    if home: 
+        screen.blit(imgSfondoPrincipale,(0,0) )
+
+        buttonColorS = "white"
+        if buttonStart.collidepoint(mPos):
+            buttonColorS = "dark grey"
+        buttonS = pygame.draw.rect(screen,buttonColorS,buttonStart)
+        screen.blit(parolaButtonStart, (SCREEN_WIDTH // 2.18, SCREEN_HEIGHT // 1.235))
+        
+        buttonColorP = "white"
+        if buttonPersonaggi.collidepoint(mPos):
+            buttonColorP = "dark grey"
+        buttonP = pygame.draw.rect(screen,buttonColorP,buttonPersonaggi)
+        screen.blit(parolaButtonPersonaggi, (SCREEN_WIDTH // 2.4, SCREEN_HEIGHT // 1.475))
+        
+    #se ci troviamo nel gioco
+    elif personaggi:
+        screen.blit(imgSfondoPersonaggi,(0,0) )
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                home = True 
     
-    buttonColorP = "white"
-    if buttonPersonaggi.collidepoint(mPos):
-        buttonColorP = "grey"
-    buttonP = pygame.draw.rect(screen,buttonColorP,buttonPersonaggi)
-    screen.blit(parolaButtonPersonaggi, (SCREEN_WIDTH // 2.4, SCREEN_HEIGHT // 1.475))
-    
+    else:
+        screen.fill("red")
+        
     pygame.display.flip()
-
+    clock.tick(60)
 
 pygame.quit()
