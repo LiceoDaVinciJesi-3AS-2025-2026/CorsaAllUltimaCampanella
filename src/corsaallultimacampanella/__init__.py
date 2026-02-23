7b10e43d1c7620e0d215b953d9c3937a05f05118
vio <viola.alfieri09@gmail.com>
2026-02-23 20:01:23 +0100

    Update __init__.py
    aggiunto immagini rpof

--- src/corsaallultimacampanella/__init__.py
+++ src/corsaallultimacampanella/__init__.py
@@ -12,7 +12,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("corsa all'ultima campanella!")
 
 #bottone per comicnicare
-buttonStart = pygame.Rect(SCREEN_WIDTH // 2.399, SCREEN_HEIGHT // 1.235, 280, 80)
+buttonInfo = pygame.Rect(SCREEN_WIDTH // 2.45, SCREEN_HEIGHT // 1.235, 310, 80)
 
 #bottone per visualizzare i personaggi
 buttonPersonaggi = pygame.Rect(SCREEN_WIDTH // 2.4, SCREEN_HEIGHT // 1.48, 280, 80)
@@ -20,11 +20,28 @@ buttonPersonaggi = pygame.Rect(SCREEN_WIDTH // 2.4, SCREEN_HEIGHT // 1.48, 280, 
 imgSfondoPrincipale = pygame.image.load("pixelscuola.png") 
 imgSfondoPrincipale = pygame.transform.scale(imgSfondoPrincipale,(SCREEN_WIDTH,SCREEN_HEIGHT))
 
-imgSfondoPersonaggi = pygame.image.load("portapersonaggi.png")
+imgSfondoPersonaggi = pygame.image.load("sfondopersonaggi.png")
 imgSfondoPersonaggi = pygame.transform.scale(imgSfondoPersonaggi,(SCREEN_WIDTH,SCREEN_HEIGHT))
 
+imgItaliano = pygame.image.load("pixelita.png") 
+imgItaliano = pygame.transform.scale(imgItaliano,(350,350))
+imgStofilo = pygame.image.load("pixelstofilo.png") 
+imgStofilo = pygame.transform.scale(imgStofilo,(350,350))
+imgArte = pygame.image.load("pixelarte.png") 
+imgArte = pygame.transform.scale(imgArte,(380,380))
+imgInglese = pygame.image.load("pixeling.png") 
+imgInglese = pygame.transform.scale(imgInglese,(370,370))
+imgMatematica = pygame.image.load("pixelmate.png") 
+imgMatematica = pygame.transform.scale(imgMatematica,(350,350))
+imgFisica = pygame.image.load("pixelfisica.png") 
+imgFisica = pygame.transform.scale(imgFisica,(350,350))
+imgScienze = pygame.image.load("pixelscienze.png") 
+imgScienze = pygame.transform.scale(imgScienze,(380,380))
+imgInformatica = pygame.image.load("pixelinfo.png") 
+imgInformatica = pygame.transform.scale(imgInformatica,(350,350))
+
 Normalfont = pygame.font.SysFont('Impact', 60)
-parolaButtonStart = Normalfont.render("start", True, "black")
+parolaButtonInfo = Normalfont.render("informazioni", True, "black")
 parolaButtonPersonaggi = Normalfont.render("personaggi", True, "black")
 
 clock = pygame.time.Clock()
@@ -47,7 +64,7 @@ while running:
                 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1: #click sinistro 
-                if home and buttonStart.collidepoint(mPos):
+                if home and buttonInfo.collidepoint(mPos):
                     home = False
                     personaggi = False
         
@@ -60,13 +77,15 @@ while running:
     #se ci troviamo nella schermata home
     if home: 
         screen.blit(imgSfondoPrincipale,(0,0) )
-
-        buttonColorS = "white"
-        if buttonStart.collidepoint(mPos):
-            buttonColorS = "dark grey"
-        buttonS = pygame.draw.rect(screen,buttonColorS,buttonStart)
-        screen.blit(parolaButtonStart, (SCREEN_WIDTH // 2.14, SCREEN_HEIGHT // 1.235))
         
+        #bottone start
+        buttonColorI = "white"
+        if buttonInfo.collidepoint(mPos):
+            buttonColorI = "dark grey"
+        buttonI = pygame.draw.rect(screen,buttonColorI,buttonInfo)
+        screen.blit(parolaButtonInfo, (SCREEN_WIDTH // 2.445, SCREEN_HEIGHT // 1.235))
+        
+        #bottone personaggi
         buttonColorP = "white"
         if buttonPersonaggi.collidepoint(mPos):
             buttonColorP = "dark grey"
@@ -75,7 +94,23 @@ while running:
         
     #se ci troviamo nel gioco
     elif personaggi:
-        screen.blit(imgSfondoPersonaggi,(0,0) )
+        screen.blit(imgSfondoPersonaggi,(0,0))
+        #inserisco immagine prof di italiano nella schermata dei personaggi
+        screen.blit(imgItaliano, (48, 90))
+        #inserisco immagine prof di storia e filosofia nella schermata dei personaggi
+        screen.blit(imgStofilo, (420, 100))
+        #inserisco immagine prof di arte nella schermata dei personaggi
+        screen.blit(imgArte, (770, 80))
+        #inserisco immagine prof di inglese nella schermata dei personaggi
+        screen.blit(imgInglese, (1150, 80))
+        #inserisco immagine prof di matematica nella schermata dei personaggi
+        screen.blit(imgMatematica, (48, 515))
+        #inserisco immagine prof di fisica nella schermata dei personaggi
+        screen.blit(imgFisica, (420, 510))
+        #inserisco immagine prof di scienze nella schermata dei personaggi
+        screen.blit(imgScienze, (770, 490))
+        #inserisco immagine prof di informatica nella schermata dei personaggi
+        screen.blit(imgInformatica, (1150, 530))
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
@@ -88,3 +123,4 @@ while running:
     clock.tick(60)
 
 pygame.quit()
+
